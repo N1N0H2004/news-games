4ee86fa2127cd42d712d0484e8aadc0c167a076d
Ninoh van Dijke <47174@hoornbeeck.nl>
2024-02-26 11:14:23 +0100

    edit - create
    Gefixd dat je nu bij edit ook de oude values ziet.

--- resources/views/artikels/forum.blade.php
+++ resources/views/artikels/forum.blade.php
@@ -1,17 +1,17 @@
     @csrf
 
-    <strong>Title: <x-text-input type="text" name="titel" field="titel" placeholder="Title..." class="w-full border-black" autocomplete="off" :value="@old('titel')"></x-text-input> </strong> <br> <br>
+    <strong>Title: <x-text-input type="text" name="titel" field="titel" placeholder="Title..." class="w-full border-black" autocomplete="off" :value="$artikel->titel"></x-text-input> </strong> <br> <br>
     @error('title')
     <p class="text-red-500 -mt-6">{{ $message }}</p>
     @enderror
 
-    <strong>Description:  <x-text-input type="text" name="inhoud" field="inhoud" placeholder="Description..." class="w-full border-black" autocomplete="off" :value="@old('inhoud')"></x-text-input> </strong> <br> <br>
+    <strong>Description:  <x-text-input type="text" name="inhoud" field="inhoud" placeholder="Description..." class="w-full border-black" autocomplete="off" :value="$artikel->inhoud"></x-text-input> </strong> <br> <br>
 
     <strong>Device:
         <select name="tag_id" class="w-full rounded" autocomplete="off">
             <option value="">Select a Device</option>
             @foreach($tags as $tag)
-                <option value="{{ $tag->id }}" {{ old('tag_id') == $tag->id ? 'selected' : '' }}>{{ $tag->naam }}</option>
+                <option value="{{ $tag->id }}" {{ $artikel->tag_id == $tag->id ? 'selected' : '' }}>{{ $tag->naam }}</option>
             @endforeach
         </select>
     </strong> <br> <br>
@@ -20,7 +20,7 @@
         <select name="game_id" class="w-full rounded" autocomplete="off">
             <option value="">Select a Game</option>
             @foreach($games as $game)
-                <option value="{{ $game->id }}" {{ old('game_id') == $game->id ? 'selected' : '' }}>{{ $game->naam }}</option>
+                <option value="{{ $game->id }}" {{ $artikel->game_id == $game->id ? 'selected' : '' }}>{{ $game->naam }}</option>
             @endforeach
         </select>
     </strong> <br> <br>
@@ -29,7 +29,7 @@
         <select name="categorie_id" class="w-full rounded" autocomplete="off">
             <option value="">Select a Game</option>
             @foreach($categories as $categorie)
-                <option value="{{ $categorie->id }}" {{ old('categorie_id') == $categorie->id ? 'selected' : '' }}>{{ $categorie->naam }}</option>
+                <option value="{{ $categorie->id }}" {{ $artikel->categorie_id == $categorie->id ? 'selected' : '' }}>{{ $categorie->naam }}</option>
             @endforeach
         </select>
     </strong> <br> <br>
